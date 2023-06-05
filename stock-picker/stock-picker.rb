846ebc43e21cee82540c6ae619148aa009a647fd
Pedro Henrique <pedronutti@gmail.com>
2023-06-05 10:21:59 -0300

    Implement stock-picker algorithm

--- stock-picker/stock-picker.rb
+++ stock-picker/stock-picker.rb
@@ -1,13 +1,13 @@
-def generate_random_stock_prices
-  num_days = rand(5..10)    # Generate a random number between 5 and 10 for the number of days
-  min_price = rand(1..50)   # Generate a random number between 1 and 50 for the minimum price
-  max_price = rand(51..100) # Generate a random number between 51 and 100 for the maximum price
+def generate_random_stock_prices    #Randomly generates the arguments to be passed onto the array             
+  num_days = rand(5..10)  
+  min_price = rand(1..50)   
+  max_price = rand(51..100) 
 
   [num_days, min_price, max_price]
 end
 
 
-def generate_stock_prices(num_days, min_price, max_price)
+def generate_stock_prices(num_days, min_price, max_price)         #Pushes values to the array, using the arguments generated before
   prices = []
   num_days.times do
     price = rand(min_price..max_price)
@@ -19,4 +19,34 @@ end
 num_days, min_price, max_price = generate_random_stock_prices
 prices = generate_stock_prices(num_days, min_price, max_price)
 
+buy_index = 0
+sell_index = 0
+max_profit = -1
+best_buy_index = 0
+best_sell_index = 1
+
 p "These are the prices #{prices} in a #{num_days} days span"
+
+prices.each_with_index do |buy_price, buy_index|                  #Stock picker algorithm
+  (buy_index + 1).upto(prices.length - 1) do |sell_index|
+    sell_price = prices[sell_index]
+
+    if sell_price > buy_price && sell_index > buy_index
+      if (sell_price - buy_price > max_profit)
+        max_profit = sell_price - buy_price
+        best_buy_index = buy_index
+        best_sell_index = sell_index
+      end
+    end
+  end  
+end
+
+if max_profit == -1
+  p "There is no profitable option for this span of days"
+else 
+  p "The best day to buy is day #{best_buy_index + 1}, and best day to sell is day #{best_sell_index + 1}, with a profit of #{max_profit}"
+end
+
+
+
+
